2095bfd43fe79c00383eee6c07efe6fd9c3b1d32
Rhuan-Rodrigues1 <gitrhuan34@gmail.com>
2023-09-08 14:34:45 -0300

    Arrumando falha no ping dos ips

--- ansible-lab/control-node/provision.sh
+++ ansible-lab/control-node/provision.sh
@@ -1,9 +1,11 @@
 #bin/sh
+
+sudo yum -y install net-tools
 sudo yum -y install epel-release
 echo "inicialização da instalação do ansible"
 sudo yum -y install ansible
 cat <<EOT >> /etc/hosts
-192.168.56.3 control-node
-192.168.56.4 app01
-192.168.56.5 db01
+192.168.1.111 control-node
+192.168.1.110 app01
+192.168.1.109 db01
 EOT
--- ansible-lab/db01/provision.sh
+++ ansible-lab/db01/provision.sh
@@ -1,5 +1,7 @@
 #bin/sh
 
-cat <<EOT >> /home/vagrant/.ssh/authorized_keys
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCnl8g7Sm2Tq6EMhl7jDQ8aprrwvKNjMN4MiwWLksIGSywYIajG5U1+i/SKU5h9QKRf/nWqMYWmgTj1s05K48cH6fpVwNdT9WaiLDdmDxMlPuDijbO465Rbb2DhmAjd08mfqhETx9fF2plx/UV/qeg3X/jlqJL6g8+fcNwKnj9gD++prMMxDGn3XhXqhm3RLytRmTnhup/m1BZxdJjMW3OXgqE3q2N6ZYZBrylOAni5UvAamvRRNcoGLO9TSvADi0annN0aPx4FAuuWXAoyBjIT/cUJxs2HDxVj4QxC7T0Dvhyjt2kc8G5qAYpYetT1pPJdR+ehUQQcrnOGknoonW1 vagrant@localhost.localdomain
-EOT
+sudo yum -y install net-tools
+
+#cat <<EOT >> /home/vagrant/.ssh/authorized_keys
+#ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCnl8g7Sm2Tq6EMhl7jDQ8aprrwvKNjMN4MiwWLksIGSywYIajG5U1+i/SKU5h9QKRf/nWqMYWmgTj1s05K48cH6fpVwNdT9WaiLDdmDxMlPuDijbO465Rbb2DhmAjd08mfqhETx9fF2plx/UV/qeg3X/jlqJL6g8+fcNwKnj9gD++prMMxDGn3XhXqhm3RLytRmTnhup/m1BZxdJjMW3OXgqE3q2N6ZYZBrylOAni5UvAamvRRNcoGLO9TSvADi0annN0aPx4FAuuWXAoyBjIT/cUJxs2HDxVj4QxC7T0Dvhyjt2kc8G5qAYpYetT1pPJdR+ehUQQcrnOGknoonW1 vagrant@localhost.localdomain
+#EOT
